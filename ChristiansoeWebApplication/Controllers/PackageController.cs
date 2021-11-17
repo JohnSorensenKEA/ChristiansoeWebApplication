00ef10aae9ecabbeb69002a6afce850ff21781d2
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2021-11-17 08:17:48 +0100

    Add project files.

---
+++ ChristiansoeWebApplication/Controllers/PackageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChristiansoeWebApplication.Controllers
+{
+    public class PackageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
